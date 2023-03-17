f6406815946b1c7cd68d120c88e18b11a991fe28
Adriano Supeleto <adriano.supeleto.17@gmail.com>
2023-03-17 16:12:24 -0300

    Finalização módulo Pagamento

--- src/services/NSE.Pedidos.API/Configuration/SwaggerConfig.cs
+++ src/services/NSE.Pedidos.API/Configuration/SwaggerConfig.cs
@@ -15,7 +15,7 @@ namespace NSE.Pedidos.API.Configuration
                 {
                     Title = "NerdStore Enterprise Pedidos API",
                     Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
-                    Contact = new OpenApiContact() { Name = "Adriano Supeleto", Email = "adrianosupeleto@hotmail.com" },
+                    Contact = new OpenApiContact() {Name = "Eduardo Pires", Email = "contato@desenvolvedor.io"},
                     License = new OpenApiLicense() {Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
                 });
 
